d1dbde3f77c6fc5f6b6e5d9d82f3608ce4994cc5
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-05-26 16:55:57 -0500

    displays high low

--- Public/Scripts/scripts.js
+++ Public/Scripts/scripts.js
@@ -4,7 +4,7 @@ function onReady() {
   $("#playDiv").hide();
   $("#resultsDiv").hide();
   $('.setupBtn').on('click', startGame);
-
+  $('#submitButton').on('click', getResults);
 }
 
 
@@ -13,5 +13,61 @@ function startGame() {
   $('#setupDiv').hide();
   $('#playDiv').show();
 
+  var requestObject = {
+    maxNumber: max
+  };
+
+  $.ajax({
+    type: 'POST',
+    url: '/setNumber',
+    data: requestObject,
+    success: function(response) {
+      console.log(response);
+    }
+  });
+
   console.log($(this).text());
+}
+
+function getResults() {
+  var requestObject = {
+    requestArray: []
+  };
+  //loop through inputs
+  for (var i = 0; i < $('#playDiv > input').length; i++) {
+    requestObject.requestArray[i] = $('#playDiv > input').eq(i).val();
+
+  }
+  $('#playDiv > input').val('');
+  $.ajax({
+    type: 'POST',
+    url: '/checkGuess',
+    data: requestObject,
+    success: function(response) {
+      console.log(response);
+      for (var i = 0; i < response.responseArray.length; i++) {
+        console.log(response.responseArray[i]);
+        $('#playDiv > input').eq(i).attr('placeholder', interpretResults(response.responseArray[i].result));
+      }
+
+    }
+  });
+}
+
+function interpretResults(result) {
+  switch (result) {
+    case -1:
+      return "Too low.";
+    case 1:
+      return "Too high.";
+    case 0:
+      winner();
+      break;
+    default:
+      break;
+  }
+}
+
+function winner() {
+
 }
--- server.js
+++ server.js
@@ -3,13 +3,57 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 
-app.use(express.static('public'));
+//random number
+var number = 0;
 
+//uses
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//listen
 app.listen(3000, function() {
   console.log('server on 3000');
 });
 
+//other stuff
 app.get('/', function(req, res) {
-  console.log('Hi I"m working');
   res.sendFile(path.resolve('views/index.html'));
 });
+
+app.post('/setNumber', function(req, res) {
+  number = generateRandomNumber(req.body.maxNumber);
+  res.send('number set');
+});
+
+app.post('/checkGuess', function(req, res) {
+  var responseObject = {
+    responseArray: []
+  };
+  //loop through requesr array
+  for (var i = 0; i < req.body.requestArray.length; i++) {
+    responseObject.responseArray[i] = {
+      result: compareGuesses(Number(req.body.requestArray[i]))
+    };
+  }
+  res.send(responseObject);
+});
+
+//number generator
+function generateRandomNumber(max) {
+  return Math.round(Math.random() * max);
+}
+
+function compareGuesses(guess) {
+  if (guess === number) {
+    return 0;
+  } else if (guess > number) {
+    return 1;
+  } else {
+    return -1;
+  }
+}
+
+
+// look up .map
